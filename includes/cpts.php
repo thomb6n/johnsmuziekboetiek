c2e1b2b732e43dae2ed9f64026fc9894e622c113
Tom de Visser <tom@stuurlui.nl>
2024-02-24 01:06:23 +0100

    Remove single for reviews

--- includes/cpts.php
+++ includes/cpts.php
@@ -5,7 +5,8 @@ toms_register_post_type(
 	__( 'Review', 'toms' ),
 	__( 'Reviews', 'toms' ),
 	array(
-		'menu_icon' => 'dashicons-smiley',
-		'supports'  => array( 'title' ),
+		'menu_icon'          => 'dashicons-smiley',
+		'supports'           => array( 'title' ),
+		'publicly_queryable' => false,
 	)
 );
