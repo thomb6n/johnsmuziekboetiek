64c39f9b6ad56879827620f0f5eb29ac54201419
Tom de Visser <tom@stuurlui.nl>
2024-02-28 16:11:01 +0100

    Added schema for SearchAction

--- includes/yoast.php
+++ includes/yoast.php
@@ -6,7 +6,13 @@ function toms_modify_organization_schema( $data ) {
 	$data['email']     = 'info@johnsmuziekboetiek.nl';
 	$data['vatID']     = 'NL003331855B55';
 
+	var_dump( $data );
+
 	return $data;
 }
-
 add_filter( 'wpseo_schema_organization', 'toms_modify_organization_schema', 999, 1 );
+
+function toms_schema_change_search_url() {
+	return 'https://johnsmuziekboetiek.nl/tweedehands-cds-boeken-films-games/?_product_search={search_term_string}';
+}
+add_filter( 'wpseo_json_ld_search_url', 'toms_schema_change_search_url' );
