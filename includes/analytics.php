5d6fc2bd2ef1f3845b206acf25c03cd2684d0b89
Tom de Visser <tom@stuurlui.nl>
2024-02-29 16:45:18 +0100

    Added more information to the purchase conversion

--- includes/analytics.php
+++ includes/analytics.php
@@ -6,14 +6,18 @@ function toms_track_purchase_conversion( $order_id ) {
 			return;
 	}
 
-	$order       = wc_get_order( $order_id );
-	$order_total = $order->get_total();
+	$order          = wc_get_order( $order_id );
+	$order_total    = $order->get_total();
+	$order_shipping = $order->get_shipping_total();
+	$order_tax      = $order->get_total_tax();
 	?>
 	<script>
 		gtag('event', 'purchase', {
 			"transaction_id": "<?php echo $order->get_order_number(); ?>",
 			"value": <?php echo $order_total; ?>,
-			"currency": "<?php echo $order->get_currency(); ?>"
+			"currency": "<?php echo $order->get_currency(); ?>",
+			"shipping": <?php echo $order_shipping; ?>,
+			"tax": <?php echo $order_tax; ?>
 		});
 	</script>
 	<?php
